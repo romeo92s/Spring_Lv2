730af86cf9b8b3879818f73e4eb5297e7b033762
JaeHeon Lee <romeo92s@naver.com>
2022-09-18 01:45:41 +0900

    start spring_basic

---
+++ src/test/java/hello/core/SpringLv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLv2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
